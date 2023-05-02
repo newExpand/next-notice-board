85fa3ea0858e8b051ae6a6abbf383105666e8382
newExpand <magin0auto@gmail.com>
2023-05-02 21:44:26 +0900

    동적라우팅 체험해보기

---
+++ app/detail/[ID]/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+
+interface DetailProps {
+    params?: {
+        ID?: string;
+    };
+    searchParams?: {
+        search?: string;
+    };
+}
+
+const Detail = async ({ params }: DetailProps) => {
+    const db = (await connectDB).db(process.env.DB_NAME);
+    let result = await db.collection(process.env.DB_COLLECTION_NAME).findOne({
+        _id: new ObjectId(params?.ID),
+    });
+
+    console.log();
+
+    return (
+        <div>
+            <h4>상세페이지</h4>
+            <h4>{result.title}</h4>
+            <p>{result.content}</p>
+        </div>
+    );
+};
+
+export default Detail;
--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connectDB } from "@/util/database";
 import { PostType } from "@/util/database";
+import Link from "next/link";
 
 const List = async () => {
     const db = (await connectDB).db(process.env.DB_NAME);
@@ -9,10 +10,10 @@ const List = async () => {
     return (
         <div className="list-bg">
             {result.map((item: PostType) => (
-                <div className="list-item" key={item._id}>
+                <Link className="list-item" href={`/detail/${item._id}`} key={item._id}>
                     <h4>{item.title}</h4>
                     <p>{item.content}</p>
-                </div>
+                </Link>
             ))}
         </div>
     );
