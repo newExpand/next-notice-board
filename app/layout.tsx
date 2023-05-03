23234360221cf570e3f28603171c4ca98a6c8acd
newExpand <magin0auto@gmail.com>
2023-05-03 22:36:58 +0900

    navbar 추가 및 수정페이지 버튼 클릭시 수정요청

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -11,7 +12,15 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="ko">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <div className="navbar">
+                    <Link href="/" className="logo">
+                        SMforum
+                    </Link>
+                    <Link href="/list">List</Link>
+                </div>
+                {children}
+            </body>
         </html>
     );
 }
--- pages/api/edit.ts
+++ pages/api/edit.ts
@@ -4,16 +4,16 @@ import { ObjectId } from "mongodb";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "POST") {
+        const client = await connectDB;
+        const db = await client.db(process.env.DB_NAME);
+        const { _id, title, content } = req.body;
+
+        const result = await db
+            .collection(process.env.DB_COLLECTION_NAME)
+            .updateOne({ _id: new ObjectId(_id) }, { $set: { title, content } });
+
+        res.redirect(302, "/list");
         try {
-            const client = await connectDB;
-            const db = await client.db(process.env.DB_NAME);
-            const { _id, title, content } = req.body;
-
-            const result = await db
-                .collection(process.env.DB_COLLECTION_NAME)
-                .updateOne({ _id: new ObjectId(_id) }, { $set: { title, content } });
-
-            res.redirect(302, "/list");
         } catch (err) {}
     }
 }
