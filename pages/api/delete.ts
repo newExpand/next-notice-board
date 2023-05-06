a79b6b7cee972d43c063ef07db7725b43ddcfa9a
newExpand <magin0auto@gmail.com>
2023-05-07 02:20:44 +0900

    로그인 후 로그인유저와 게시물 글쓴이가 같은 경우 삭제 구현

--- pages/api/delete.ts
+++ pages/api/delete.ts
@@ -1,22 +1,30 @@
+import { getServerSession } from "next-auth";
 import { connectDB } from "@/util/database";
 import { NextApiRequest, NextApiResponse } from "next";
 import { ObjectId } from "mongodb";
+import { authOptions } from "./auth/[...nextauth]";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "POST") {
+        const session = await getServerSession(req, res, authOptions);
         const client = await connectDB;
         const db = await client.db(process.env.DB_NAME);
 
-        const deleteDB = await db
+        const findUser = await db
             .collection(process.env.DB_COLLECTION_NAME)
-            .deleteOne({ _id: new ObjectId(req.body) });
+            .findOne({ _id: new ObjectId(req.body) });
 
-        const result = await db
-            .collection(process.env.DB_COLLECTION_NAME)
-            .find()
-            .toArray();
-
-        return res.status(200).json(result);
-        // res.status(200).json("삭제 완료");
+        if (findUser.author === session?.user?.email) {
+            const deleteDB = await db
+                .collection(process.env.DB_COLLECTION_NAME)
+                .deleteOne({ _id: new ObjectId(req.body) });
+            const result = await db
+                .collection(process.env.DB_COLLECTION_NAME)
+                .find()
+                .toArray();
+            return res.status(200).json(result);
+        } else {
+            return res.status(500).json("글쓴이와 작성자가 불일치 합니다.");
+        }
     }
 }
