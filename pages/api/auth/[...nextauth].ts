6e99e4febcdbffc279dd73b80f1cc7d3febc6a2e
newExpand <magin0auto@gmail.com>
2023-05-06 23:58:32 +0900

    Oauth session 기능 쓰기 위해 DB adapter 설정 및 글작성시 db에 쓴 이메일 계정 추가

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,5 @@
+import { connectDB } from "@/util/database";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
@@ -10,6 +12,7 @@ export const authOptions: AuthOptions = {
     ],
 
     secret: process.env.NEXTAUTH_SECRET,
+    adapter: MongoDBAdapter(connectDB),
 };
 
 export default NextAuth(authOptions);
--- pages/api/post/new.ts
+++ pages/api/post/new.ts
@@ -1,7 +1,15 @@
+import { getServerSession } from "next-auth";
 import { connectDB } from "@/util/database";
 import { NextApiRequest, NextApiResponse } from "next";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const session = await getServerSession(req, res, authOptions);
+
+    if (session) {
+        req.body.author = session.user?.email;
+    }
+
     if (req.method === "POST") {
         if (req.body.title.trim() == "") {
             return res.status(500).json("제목써라");
